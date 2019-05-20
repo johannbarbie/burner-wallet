bbd1cccfce9307bd9ab7524a3b86ca0a64d113e2
Alex Lunyov <isuntc@gmail.com>
2019-05-21 01:36:23 +0800

    Use slim dapparatus (-1.57 MB gzipped)

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -265,6 +265,10 @@ module.exports = function(webpackEnv) {
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         'bn.js': require.resolve('bn.js'),
+        'leap-core': require.resolve('leap-core'),
+        'elliptic': require.resolve('elliptic'),
+        'lodash$': require.resolve('lodash'),
+        'lodash/map': require.resolve('lodash/map'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
